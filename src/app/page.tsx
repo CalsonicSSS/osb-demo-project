6f834b8429f28106f52b1847ac86102552d59e62
CalsonicSKC1005 <calson@withautomatic.com>
2024-07-03 18:02:50 -0400

    refined on layout branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import CustomersTable from '@/components/CustomersTable'
 import ThemeToggle from '@/components/ThemeToggle'
 import { getAllCustomers } from '@/supabase/calls'
@@ -6,20 +7,26 @@ export default async function Index() {
   const customers = await getAllCustomers()
 
   return (
-    <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm"></div>
-      </nav>
+    // home page scaffold layout divided into sidebar and main content
+    <div className="flex w-full">
+      {/* Sidebar */}
 
-      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
-        <main className="flex flex-1 flex-col gap-6">
-          <CustomersTable customers={customers ?? []} />
-        </main>
+      {/* Main Content */}
+      <div className="flex flex-1 flex-col items-center gap-20">
+        {/* <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+          <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm"></div>
+        </nav> */}
+
+        <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
+          <main className="flex flex-1 flex-col gap-6">
+            <CustomersTable customers={customers ?? []} />
+          </main>
+        </div>
+
+        <footer className="w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
+          <ThemeToggle />
+        </footer>
       </div>
-
-      <footer className="w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
-        <ThemeToggle />
-      </footer>
     </div>
   )
 }
---
+++ src/typings/props.ts
@@ -0,0 +1,6 @@
+export interface SidebarIconButtonProps {
+  src: string
+  alt: string
+  href: string
+  label: string
+}
